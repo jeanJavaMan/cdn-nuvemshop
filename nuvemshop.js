9d6e89d8af8150d24b41c73d93e735497bfcc673
Jeanderson Silva <jeananimax@gmail.com>
2022-05-31 21:13:34 -0300

    Update nuvemshop.js

--- nuvemshop.js
+++ nuvemshop.js
@@ -4,26 +4,11 @@
         /* JavaScript that will load the jQuery library on Google's CDN.
            We recommend this code: https://snipplr.com/view/18756/loadscript/.
            Once the jQuery library is loaded, the callback function will be executed. */
-        var script = document.createElement("script");
-        script.type = "text/javascript";
-
-        // If the browser is Internet Explorer.
-        if (script.readyState) {
-            script.onreadystatechange = function () {
-                if (script.readyState == "loaded" || script.readyState == "complete") {
-                    script.onreadystatechange = null;
-                    callback();
-                }
-            };
-            // For any other browser.
-        } else {
-            script.onload = function () {
-                callback();
-            };
-        }
-
+        var script = document.createElement('script');
         script.src = url;
-        document.getElementsByTagName("head")[0].appendChild(script);
+        script.type = 'text/javascript';
+
+        document.getElementsByTagName("HEAD")[0].appendChild(script);
 
     };
 
